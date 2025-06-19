75a901fc4316a97f51284791b7d8bc38ec9f328c
chagassilva <chagassilvati@gmail.com>
2025-06-18 22:14:04 -0300

    Enviando para o deploy api

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,19 @@
 
-// import axios from "axios"
-
-// const api = axios.create({
-
-//     baseURL: "http://localhost:3000"
-
-// })
-
-// export default api
-
-
-import axios from "axios";
+import axios from "axios"
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000"
-});
 
-export default api;
+    baseURL: "http://localhost:3000"
+
+})
+
+export default api
+
+
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000"
+// });
+
+// export default api;
